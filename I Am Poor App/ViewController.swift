350a8bce6d1f70d232fa4e1907d8f8050d1d8b7c
Alex <57699618+accountAlex@users.noreply.github.com>
2023-09-05 13:32:05 +0300

    Initial Commit

---
+++ I Am Poor App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor App
+//
+//  Created by Александр Заречкин on 05.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
