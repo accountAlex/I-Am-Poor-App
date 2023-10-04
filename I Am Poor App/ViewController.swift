9f0048087e95f7673aaa9ff16e70d44910799699
Alex <57699618+accountAlex@users.noreply.github.com>
2023-10-04 19:37:11 +0300

    final

--- I Am Poor App/ViewController.swift
+++ I Am Poor App/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var button_tyk: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        button_tyk.addTarget(self, action: #selector(toggleDarkMode), for: .touchUpInside)
     }
+    
 
-
+    @objc func toggleDarkMode() {
+        if #available(iOS 12.0, *) {
+            if self.traitCollection.userInterfaceStyle == .light {
+                
+                lbl.text = "Barbie"
+                
+                UIApplication.shared.windows.forEach { window in
+                    window.overrideUserInterfaceStyle = .dark
+                }
+            } else {
+       
+                lbl.text = "Drive"
+           
+                UIApplication.shared.windows.forEach { window in
+                    window.overrideUserInterfaceStyle = .light
+                }
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if #available(iOS 12.0, *) {
+            if self.traitCollection.userInterfaceStyle == .light {
+          
+                lbl.text = "Barbie"
+            } else {
+               
+                lbl.text = "Drive"
+            }
+        }
+    }
 }
-
